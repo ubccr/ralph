6e3b11e0b109c591a21c5aba5ca2e28804a0a5af
John Rivera <riveraj@buffalo.edu>
2017-03-10 15:23:46 -0500

    Improve documentation, set minimum supported PHP version to 5.5.

--- src/SparqlClient.php
+++ src/SparqlClient.php
@@ -35,21 +35,20 @@ class SparqlClient
     } // __construct()
 
     /**
-     * Sets the endpoint for connecting to the SPARQL server.
+     * Sets the endpoint for connecting to the SPARQL server. A client can only
+     * connect to one endpoint at a time; any subsequent calls to this method
+     * will replace the existing endpoint.
      *
-     * @param string $uri The endpoint to set. Must conform to RFC 3986.
+     * @param string $endpoint The endpoint to set. Must conform to RFC 3986.
      *
-     * @return SparqlClient The clone of this client with the base URI set.
+     * @return SparqlClient The clone of this client with the endpoint set.
      *
      * @throws InvalidUriException If the endpoint is not a valid RFC 3986 URI.
      */
 
     public function withEndpoint($endpoint)
     {
-        if ( filter_var($endpoint, FILTER_VALIDATE_URL) === false ) {
-            throw new InvalidUriException($endpoint);
-        }
-
+        $this->validate($endpoint);
         $new = clone $this;
         $new->endpoint = $endpoint;
 
@@ -57,21 +56,26 @@ class SparqlClient
     } // withEndpoint()
 
     /**
-     * Adds a prefix that will be prepended to all queries made by this client.
-     * Allows for immutability and fluent-style method calls by returning a new
-     * copy of this client with the prefix added, leaving the original
-     * unmodified.
+     * Sets a prefix that will be prepended to all queries made by this client.
+     * More than one prefix can be set; make subsequent calls to this method as
+     * needed to add more prefixes.
      *
      * @param string $name The name of the namespace.
      * @param string $namespace The URI of the namespace.
      *
      * @return SparqlClient The clone of this client with the prefix added.
+     *
+     * @throws InvalidUriException If the namespace is not a valid RFC 3986 URI.
      */
 
     public function withPrefix($name, $namespace)
     {
+        $this->validate($namespace);
         $new = clone $this;
-        $new->prefixes[] = "PREFIX {$name}: <{$namespace}>";
+        $new->prefixes[] = [
+            'name' => $name,
+            'namespace' => $namespace
+        ];
 
         return $new;
     } // withPrefix()
@@ -98,11 +102,23 @@ class SparqlClient
         return json_decode($response->getBody(), true);
     } // query()
 
+    private function validate($uri)
+    {
+        if ( filter_var($uri, FILTER_VALIDATE_URL) === false ) {
+            throw new InvalidUriException($uri);
+        }
+    }
+
     private function process($query)
     {
-        $prefixes = implode(' ', $this->prefixes);
-        $minified = trim(preg_replace('/\s+/', ' ', $prefixes . $query));
+        $result = '';
 
-        return $minified;
+        foreach ( $this->prefixes as $prefix ) {
+            $result .= "PREFIX {$prefix['name']}: <{$prefix['namespace']}> ";
+        }
+
+        $result .= trim(preg_replace('/\s+/', ' ', $query));
+
+        return $result;
     } // process()
 } // class SparqlClient
